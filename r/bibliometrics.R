31ffc0be821189c79594c3b2afe1790e9e5adde3
Benjamin Rubinstein <benjamin.rubinstein@unimelb.edu.au>
2018-03-06 00:41:20 +1100

    Extract citations from Google Scholar

---
+++ r/bibliometrics.R
@@ -0,0 +1,37 @@
+# script : bibliometrics.R
+# author : Benjamin Rubinstein
+# about  : extracts citation counts from a Google Scholar profile.
+#          RefManageR should do this with ReadGS() but doesn't work.
+
+library(httr)
+library(xml2)
+library(RefManageR)
+library(stringr)
+
+# Collects up to 100 most-cited publications from a Google Scholar
+# profile, extracting authorYearTitle keys and citation counts. Given
+# - Scholar ID
+# - Encocding 
+# Returns matrix of two cols. First being the key, second citation count.
+ScholarCites <- function(scholar.id, .Encoding="UTF-8")
+{
+  limit <- 100  # not more than 100
+  ps <- ifelse(limit <= 20, 20, 100)
+  .params <- list(hl="en", user=scholar.id, oe=.Encoding, 
+                  pagesize=ps, view_op="list_works", cstart=0)
+  uri <- "http://scholar.google.com/citations"
+  doc <- GET(uri, query=.params)
+  doc <- content(doc, type="text/html", encoding="UTF-8")
+  x <- xml_find_all(doc, "//tr[@class=\"gsc_a_tr\"]")
+  cites <- xml_text(xml_find_all(x, "//td/a[@class=\"gsc_a_ac gs_ibl\"]"))
+  cites <- sapply(as.integer(cites), function(y) ifelse(is.na(y), 0, y))
+  titles <- xml_text(xml_find_all(x, "//td/a[@class=\"gsc_a_at\"]"))
+  titles <- sapply(titles, function(y) strsplit(gsub("[^[:alnum:] ]", "", tolower(y)), " +")[[1]][[1]])
+  years <- xml_text(xml_find_all(x, "//td/span[@class=\"gsc_a_h gsc_a_hc gs_ibl\"]"))
+  authors <- xml_text(xml_find_all(x, "//td/div[@class=\"gs_gray\"]"))
+  authors <- authors[seq(from=1, by=2, to=length(authors))]
+  authors <- sapply(authors, function(a) strsplit(tolower(a), ", ", fixed=T)[[1]][1])
+  authors <- sapply(authors, word, -1)
+  keys <- apply(cbind(authors, years, titles), 1, paste, collapse="", sep="")
+  matrix(c(keys, cites), ncol=2, byrow=F)
+}
